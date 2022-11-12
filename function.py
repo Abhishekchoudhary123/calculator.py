31ce75a23a6a2037f311f5be3ce2e24bb39d6692
Abhishekchoudhary123 <114806059+Abhishekchoudhary123@users.noreply.github.com>
2022-11-12 13:17:08 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,25 @@
+#function to add numbers
+def add(num1, num2):
+    return num1 + num2
+
+#function to subtract numbers
+def subtract(num1, num2):
+    return num1 - num2
+
+#function to multiply numbers
+def multiply(num1, num2):
+    return num1 * num2
+
+#function to devide numbers
+def divide(num1, num2):
+    return num1 / num2
+
+print("please select operation -\n" \
+         "1. add\n" \
+         "2. subtract\n" \
+         "3. multiply\n" \
+         "4. divide\n")
+
+
+     
+
